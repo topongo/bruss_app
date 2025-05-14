00fe04a45886bfb14ecfe0cb96c0f11f08fd2b65
Lorenzo Bodini <lorenzo.bodini@prabo.org>
2025-05-14 18:15:51 +0200

    fix: make RouteIcon more versatile

--- lib/ui/pages/map/sheet/route_icon.dart
+++ lib/ui/pages/map/sheet/route_icon.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:bruss/data/route.dart' as br;
 import 'package:flutter/material.dart';
 
 class RouteIcon extends StatelessWidget {
@@ -7,18 +8,28 @@ class RouteIcon extends StatelessWidget {
   final String label;
   final Color color;
 
+  factory RouteIcon.fromRoute(br.Route route) {
+    return RouteIcon(
+      label: route.code,
+      color: route.color,
+    );
+  }
+
   @override 
   Widget build(BuildContext context) {
+    final tColor = color.computeLuminance() < 0.5 ? Colors.white : Colors.black;
+    final textStyle = Theme.of(context).textTheme.titleLarge?.copyWith(color: tColor) ??
+      TextStyle(color: tColor, fontSize: 16);
     return Container(
-      height: 40,
       width: 40,
+      height: 40,
       decoration: BoxDecoration(
         color: color,
         borderRadius: const BorderRadius.all(Radius.circular(12.0)),
       ),
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
-        child: Text(label, style: Theme.of(context).textTheme.titleLarge, textAlign: TextAlign.center),
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Center(child: Text(label, style: textStyle)),
       ),
     );
   }
