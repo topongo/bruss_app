6e4f94add70615f3b5fec929ff91f12810a937d7
Lorenzo Bodini <lorenzo.bodini@prabo.org>
2025-05-19 10:55:34 +0200

    change: switch to asset style

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'dart:convert';
+import 'dart:io';
 
-import 'package:bruss/data/trip_updates.dart';
+import 'package:bruss/api.dart';
 import 'package:bruss/ui/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart';
 import 'package:vector_map_tiles/vector_map_tiles.dart';
-import 'ui/pages/map/map.dart';
+import 'package:vector_tile_renderer/vector_tile_renderer.dart';
 import 'error.dart';
+import 'ui/pages/map/map.dart';
+
+late Style mapStyle;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,12 +24,28 @@ void main() async {
     ErrorHandler.onPlatformError(error, stack);
     return true;
   };
-  mapStyle = await StyleReader(
-    uri: 'https://github.com/immich-app/immich/raw/84da9abcbcb853dd853e2995ec944fc6e934da39/server/resources/style-dark.json',
-        // logger: const Logger.console()
-  ).read();
+  // mapStyle = await StyleReader(
+  //   uri: 'https://github.com/immich-app/immich/raw/84da9abcbcb853dd853e2995ec944fc6e934da39/server/resources/style-dark.json',
+  //       // logger: const Logger.console()
+  // ).read();
 
-  final TripUpdates x = TripUpdates.fromJson(jsonDecode('{"id":"0004203242024090920250612","delay":0,"last_stop":474,"next_stop":377,"area":"u","bus_id":null}'));
+  final rawStyle = await rootBundle.loadString("assets/map_style.json");
+  final Map<String, dynamic> dataStyle = jsonDecode(rawStyle);
+  final style = Style(
+    theme: ThemeReader().read(dataStyle), 
+    providers: TileProviders(
+      {"immich-map": NetworkVectorTileProvider(
+        type: TileProviderType.vector,
+        urlTemplate: "https://api-l.cofractal.com/v0/maps/vt/overture/{z}/{x}/{y}",
+        maximumZoom: 15,
+        minimumZoom: 7,
+      )}
+    ),
+  );
+  mapStyle = style;
+
+  MapInteractor.init();
+
   runApp(App());
 }
 
